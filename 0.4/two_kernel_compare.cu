6f6d4943bc178deb4bc0ce08f7e3424e2be09abf
Clay <ccs96307@gmail.com>
2025-05-28 00:29:04 +0800

    [Update] Events

--- 0.4/two_kernel_compare.cu
+++ 0.4/two_kernel_compare.cu
@@ -55,14 +55,43 @@ int main() {
     checkCudaError(cudaMemcpyAsync(d_A, h_A, DATA_SIZE, cudaMemcpyHostToDevice), "cudaMemcpyAsync d_A to stream1");
     checkCudaError(cudaMemcpyAsync(d_B, h_B, DATA_SIZE, cudaMemcpyHostToDevice), "cudaMemcpyAsync d_B to stream2");
  
-    // Launch kernel
+    // Count time
+    cudaEvent_t start, stop;
+    checkCudaError(cudaEventCreate(&start), "cudaEventCreate start");
+    checkCudaError(cudaEventCreate(&stop), "cudaEventCreate stop");
+
+    // Launch kernel (stream1)
+    checkCudaError(cudaEventRecord(start, stream1), "cudaEventRecord start");
+
     streamKernel1<<<1, N, 0, stream1>>>(d_A);
     checkCudaError(cudaGetLastError(), "streamKernel1 launch");
     printf("streamKernel1 launched in stream1\n");
 
+    checkCudaError(cudaEventRecord(stop, stream1), "cudaEventRecord stop");
+    checkCudaError(cudaEventSynchronize(stop), "cudaEventSynchronize stop");
+
+    float milliseconds = 0;
+    checkCudaError(cudaEventElapsedTime(&milliseconds, start, stop), "cudaEventElapsedTime stream1");
+    printf("GPU kernel (stream2) time: %f ms\n", milliseconds);
+
+    // Count time
+    // cudaEvent_t start, stop;
+    // checkCudaError(cudaEventCreate(&start), "cudaEventCreate start");
+    // checkCudaError(cudaEventCreate(&stop), "cudaEventCreate stop");
+
+    // Launch kernel (stream2)
+    checkCudaError(cudaEventRecord(start, stream2), "cudaEventRecord start");
+
     streamKernel2<<<1, N, 0, stream2>>>(d_B);
     checkCudaError(cudaGetLastError(), "streamKernel2 launch");
-    printf("streamKernel2 launched in stream1\n");
+    printf("streamKernel2 launched in stream2\n");
+
+    checkCudaError(cudaEventRecord(stop, stream2), "cudaEventRecord stop");
+    checkCudaError(cudaEventSynchronize(stop), "cudaEventSynchronize stop");
+
+    milliseconds = 0;
+    checkCudaError(cudaEventElapsedTime(&milliseconds, start, stop), "cudaEventElapsedTime stream2");
+    printf("GPU kernel (stream2) time: %f ms\n", milliseconds);
 
     // Copy result from device to host
     checkCudaError(cudaMemcpyAsync(h_A, d_A, DATA_SIZE, cudaMemcpyDeviceToHost), "cudaMemcpyAsync d_A to host from stream1");
